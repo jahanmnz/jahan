10019d294b9f9354c38d6644e637b02bcafb782c
Nurmuhammad <dushanbe.student@UCA.corp>
2023-07-18 18:38:21 +0500

    file modified

---
+++ assingment.py
@@ -0,0 +1,8 @@
+year = 360
+hours = 24 
+minutes = 60 
+seconds =60 
+
+print (Hours in a year ,(year * hours))
+print (Minutes in a year ,(year *hours* minutes ))
+print (Seconds in a year ,(year *hours* minutes * seconds))
