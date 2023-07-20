4363f4592625d0a5abc409adacb1feed02d291fb
Nurmuhammad <dushanbe.student@UCA.corp>
2023-07-20 19:55:27 +0500

    new file has been added

--- opreators.py
+++ opreators.py
@@ -1,6 +1,24 @@
-x = 20 
-y = 30 
-print(x<y)
+x = ("apple", "banana")
+y = ("apple", "banana") 
+z = x
+print (x is z)
+print (x is y)
+print (x == x)
+
+
+#x = 9 
+
+#print(x < 5 and x  < 10)
+
+#x = 9 
+
+#print(x < 5 or x  > 10)
+
+
+
+#x = 20 
+#y = 50  
+#print(x!=y)
 
 
 #x = 5 
@@ -10,4 +28,4 @@ print(x<y)
 
 #x =8
 #y = 10 
-#print ( x *y)
+#print ( x*y)
